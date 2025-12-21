738255edd17f9008cf217be11ffe1bab483a8c41
astsuganya <141382963+astsuganya@users.noreply.github.com>
2025-12-21 20:51:06 +0700

    feature(theme): กำหนด color และ theme

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color background = Color(0xFF000E33);
+  static const Color primaryBrand = Color(0xFF004AAD);
+  static const Color secondaryBrand = Color(0xFFFFDA08);
+  static const Color surface = Color(0xFFFFFFFF);
+  static const Color scale = Color(0xFFD9D9D9);
+  static const Color textPrimary = Color(0xFF000000);
+  static const Color textSecondary = Color(0xFF828282);
+  static const Color textDisabled = Color(0xFF9E9E9E);
+  static const Color submit = Color(0xFF004AAD);
+  static const Color status = Color(0xFF2AB930);
+  static const Color cancel = Color(0xFFDA100B);
+  static const Color warning = Color(0xFFFFCD29);
+}
+
