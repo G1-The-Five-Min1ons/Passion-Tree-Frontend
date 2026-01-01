cc7851e2bbd31dac3e18bc633b2134d54883aa46
myminit <mintdot77@gmail.com>
2026-01-02 02:46:08 +0700

    feature(typography): แก้ขนาดหรือเพิ่ม littlesmall มีขนาด8 สำหรับใช้กับ bar แก้วิธีการเรียกtypoในmain ทั้งเรียกจากthemeและจากtypoโดยตรง

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:passion_tree_frontend/core/theme/typography.dart';
 import 'core/theme/theme.dart';
 
 void main() {
@@ -74,29 +75,10 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
+           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You have pushed the button this many times:'),
-        Text(
-          'TEST',
-          style: const TextStyle(
-            fontFamily: 'PressStart2P',
-            fontSize: 20,
-          ),
-        ),
+            Text('TEST', style: Theme.of(context).textTheme.displaySmall),
+            Text('button:', style: AppPixelTypography.title),
           ],
         ),
       ),
