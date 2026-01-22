ea9ddcff3fe1b0f135fb7c75654e914b0877c008
astsuganya <141382963+astsuganya@users.noreply.github.com>
2026-01-23 02:49:21 +0700

    feature(add album popup): ทำ add album popup

--- lib/features/reflection_tree/presentation/pages/add_reflect_page.dart
+++ lib/features/reflection_tree/presentation/pages/add_reflect_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:passion_tree_frontend/core/common_widgets/bars/appbar.dart';
 import 'package:passion_tree_frontend/core/common_widgets/buttons/app_button.dart';
 import 'package:passion_tree_frontend/core/common_widgets/buttons/button_enums.dart';
-import 'package:passion_tree_frontend/core/common_widgets/icons/pixel_icon.dart';
 import 'package:passion_tree_frontend/core/theme/theme.dart';
 import 'package:passion_tree_frontend/core/theme/typography.dart';
 import 'package:passion_tree_frontend/features/reflection_tree/presentation/widgets/searchdropdown.dart';
---
+++ lib/features/reflection_tree/presentation/widgets/save_cancel.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:passion_tree_frontend/core/common_widgets/buttons/app_button.dart';
+import 'package:passion_tree_frontend/core/common_widgets/buttons/button_enums.dart';
+import 'package:passion_tree_frontend/core/theme/colors.dart';
+
+class SaveCancel extends StatelessWidget{
+  final VoidCallback onSave;
+  final VoidCallback onCancel;
+  final String saveText;
+  final String cancelText;
+
+  const SaveCancel({
+    super.key,
+    required this.onSave,
+    required this.onCancel,
+    this.saveText = 'Save',
+    this.cancelText = 'Cancel',
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+          AppButton(
+            variant: AppButtonVariant.text,
+            text: cancelText,
+            onPressed: onCancel,
+            backgroundColor: AppColors.scale,
+            textColor: Theme.of(context).colorScheme.onSurface
+          ),
+        
+        const SizedBox(width: 8),
+
+        AppButton(
+          variant: AppButtonVariant.text,
+          text: saveText,
+          onPressed: onSave,
+        ),
+      ],
+    );
+  }
+}
